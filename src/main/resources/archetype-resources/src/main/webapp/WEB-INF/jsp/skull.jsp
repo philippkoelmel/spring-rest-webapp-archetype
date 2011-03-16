69758ef78dd88e9cf4dc0187d13a7a362058180f
Philipp <me@philippkoelmel.com>
2011-03-16 23:19:19 +0100

    initial

---
+++ src/main/resources/archetype-resources/src/main/webapp/WEB-INF/jsp/skull.jsp
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+<html>
+<head>
+	<title>Skeleton</title>
+</head>
+<body>
+<h1>Skull</h1>
+Id: <c:out value="${symbol_dollar}{skull.id}"/>
+</body>
+</html>
---
+++ src/main/resources/archetype-resources/src/test/java/xml/MarshallTest.java
@@ -0,0 +1,46 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${packageInPathFormat}.xml;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import javax.xml.transform.stream.StreamResult;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Marshaller;
+import org.springframework.oxm.XmlMappingException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import ${packageInPathFormat}.skull.Skull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/test-context.xml")
+public class MarshallTest {
+	
+	@Autowired
+	Marshaller marshaller;
+	
+	private static final String XML_SKULL_RESULT="<?xml version=${symbol_escape}"1.0${symbol_escape}" encoding=${symbol_escape}"UTF-8${symbol_escape}" standalone=${symbol_escape}"yes${symbol_escape}"?><skull><id>1</id></skull>";
+	
+	@Test
+	public void marshallSkull() throws XmlMappingException, IOException {
+
+		Skull skull = new Skull();
+		skull.setId(1);
+		StringWriter writer = new StringWriter();
+		StreamResult result = new StreamResult(writer);
+		
+		marshaller.marshal(skull, result);
+		
+		assertEquals(
+				XML_SKULL_RESULT,
+				writer.toString());
+	}
+}
